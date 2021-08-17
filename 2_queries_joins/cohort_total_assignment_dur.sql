5948db5851a7b190f342d280262516b70141984e
yhupaul <yhupaul@hotmail.com>
2021-08-16 19:42:13 -0700

    add AVG

--- 2_queries_joins/cohort_total_assignment_dur.sql
+++ 2_queries_joins/cohort_total_assignment_dur.sql
@@ -1,5 +1,5 @@
 SELECT sum(assignment_submissions.duration) as total_duration
 FROM assignment_submissions
-JOIN students ON students.id = student_id
+JOIN students ON students.id = studentstu_id
 JOIN cohorts ON cohorts.id = cohort_id
 WHERE cohorts.name = 'FEB12';
