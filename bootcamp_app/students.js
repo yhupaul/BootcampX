5d23b0a54c3e68bd0fb1e58453286a677de15655
yhupaul <yhupaul@hotmail.com>
2021-08-19 07:44:13 -0700

    inject sql

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,24 +7,18 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-// pool.query(`
-// SELECT id, name, cohort_id
-// FROM students
-// LIMIT 5;
-// `)
-// .then(res => {
-//   console.log(res.rows);
-// })
-// .catch(err => console.error('query error', err.stack));
-// const args = process.argv[2]
-
-pool.query(`
+const querySrting = `
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
+pool.query(querySrting, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,17 +7,21 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const querySrting = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort 
 FROM teachers
 JOIN assistance_requests ON teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2] || 'JUL02'}%'
-ORDER BY teachers.name;
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.cohort}: ${user.teacher}`);
-  })
-}).catch(err => console.error('query error', err.stack));
+WHERE cohorts.name LIKE $1
+ORDER BY teacher;
+`;
+const cohortName = process.argv[2] || 'JUL02';
+const values = [`%${cohortName}%`];
+
+pool.query(querySrting, values)
+  .then(res => {
+    res.rows.forEach(row => {
+      console.log(`${row.cohort}: ${row.teacher}`);
+    });
+  }).catch(err => console.error('query error', err.stack));
